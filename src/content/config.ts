3d16c8ef5ea422bd85753e79043354ae91bf4ef7
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-07-29 17:42:09 +0900

    content/config: blogスキーマから不要なフィールド削除

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,10 +4,7 @@ const blogCollection = defineCollection({
   schema: z.object({
     title: z.string().max(100, "The title length must be less than or equal to 100 chars"),
     description: z.string(),
-    tags: z.array(z.string()),
-    author: z.string(),
-    authorImage: z.string().optional(),
-    authorTwitter: z.string(),
+    tags: z.array(z.string()).default([]),
     date: z.string(),
     image: z.string().optional(),
     category: z.string()
