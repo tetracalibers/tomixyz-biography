cac40d6da9250006139b9c1fc1dd538a870d9b1b
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-07-31 15:19:06 +0900

    テーマ切り替えアイコンのサイズ調整

--- src/components/ModeSwitcherBtn.svelte
+++ src/components/ModeSwitcherBtn.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
   import ModeSwitcher from "./ModeSwitcher.svelte"
-  import SvgIcon from "./SvgIcon.svelte"
 </script>
 
 <ModeSwitcher let:theme>
-  <SvgIcon>
-    {#if theme === "dark"}
+  {#if theme === "dark"}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
       <circle cx="12" cy="12" r="5"></circle>
       <line x1="12" y1="1" x2="12" y2="3"></line>
       <line x1="12" y1="21" x2="12" y2="23"></line>
@@ -15,8 +24,20 @@
       <line x1="21" y1="12" x2="23" y2="12"></line>
       <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
       <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-    {:else}
+    </svg>
+  {:else}
+    <svg
+      width="22"
+      height="22"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      viewBox="1 1.71 17.29 17.29"
+    >
       <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-    {/if}
-  </SvgIcon>
+    </svg>
+  {/if}
 </ModeSwitcher>
