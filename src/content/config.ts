2f55b0f736a5c88673040e1321f5f715f2889083
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-08-01 09:17:54 +0900

    event,postからタグ表示を削除

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,6 @@ const blogCollection = defineCollection({
   schema: z.object({
     title: z.string().max(100, "The title length must be less than or equal to 100 chars"),
     description: z.string(),
-    tags: z.array(z.string()).default([]),
     date: z.string(),
     image: z.string().optional(),
     category: z.string()
@@ -27,7 +26,6 @@ const eventCollection = defineCollection({
   schema: z.object({
     title: z.string().max(100, "The title length must be less than or equal to 100 chars"),
     description: z.string(),
-    tags: z.array(z.string()).default([]),
     date: z.string(),
     image: z.string().optional(),
     url: z.string().url().optional(),
