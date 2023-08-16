99ba443cbd4e4204f4dcecfb1c4a71a5e1116181
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-08-16 11:56:55 +0900

    blog.categoryの型定義をenumに

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ const blogCollection = defineCollection({
     description: z.string(),
     date: z.string(),
     image: z.string().optional(),
-    category: z.string()
+    category: z.enum(["essay", "tech"])
   })
 })
 
