0fc18c964b7a9e081aa9b6baf0033f91c713b2b9
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-28 06:10:20 +0900

    base URLに末尾のスラッシュを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ export default defineConfig(
     // outDir: './dist',       // When running `astro build`, path to final static output
     // publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
     site: "https://tetracalibers.github.io",
-    base: "/tomixyz-biography",
+    base: "/tomixyz-biography/",
     compressHTML: true,
     scopedStyleStrategy: "where",
     build: {
