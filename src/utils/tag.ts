846a7257a2a758c9c478439db352ed7389076e87
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-10-05 08:49:22 +0900

    タグを集める処理をユーティリティ化

--- src/utils/tag.ts
+++ src/utils/tag.ts
@@ -1,3 +1,7 @@
 export const tagToSlug = (tag: string) => {
   return tag.toLowerCase().replaceAll(/\s/g, "-")
 }
+
+export const collectTags = (collection: { data: { tags: string[] } }[]) => {
+  return [...new Set(collection.flatMap((item) => item.data.tags))]
+}
