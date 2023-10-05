f06c47bb17e951be4e8d8bf438b2a95d2ff5d101
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-10-06 08:56:30 +0900

    各タグページを多言語化対応

--- src/utils/tag.ts
+++ src/utils/tag.ts
@@ -5,3 +5,11 @@ export const tagToSlug = (tag: string) => {
 export const collectTags = (collection: { data: { tags: string[] } }[]) => {
   return [...new Set(collection.flatMap((item) => item.data.tags))]
 }
+
+const isMatchTagOf = (tags: string[]) => {
+  return (tag: string) => tags.map((tag) => tag.toLowerCase()).includes(tag.toLowerCase())
+}
+
+export const includeTag = (tag: string) => {
+  return (entry: { data: { tags: string[] } }) => isMatchTagOf(entry.data.tags)(tag)
+}
