57d06ae214faa3db72aa93fb3e2dbbfb925f1534
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-10-06 08:55:26 +0900

    ルーティングUtilityの改良

--- src/utils/i18n/routing/article-page.ts
+++ src/utils/i18n/routing/article-page.ts
@@ -1,15 +1,19 @@
 import type { CollectionEntry, ContentCollectionKey } from "astro:content"
 
-type I18nPathsArgs<C extends ContentCollectionKey> = {
-  collection: CollectionEntry<C>[]
+type PathsOptions = {
+  props?: Record<string, unknown>
+  params?: Record<string, string>
 }
 
 // [...lang]/**/[...slug].astro用
-export const i18nPaths = <C extends ContentCollectionKey>({ collection }: I18nPathsArgs<C>) => {
+export const i18nPaths = <C extends ContentCollectionKey>(
+  collection: CollectionEntry<C>[],
+  { props = {}, params = {} }: PathsOptions = {}
+) => {
   return collection.flatMap((page) => {
     const { slug } = page
     const [lang, defaultSlug] = slug.split("/")
-    const paths = { props: { page }, params: { slug: defaultSlug, lang } }
+    const paths = { props: { page, ...props }, params: { slug: defaultSlug, lang, ...params } }
     return slug.startsWith("ja") ? [paths, { ...paths, params: { ...paths.params, lang: undefined } }] : paths
   })
 }
--- src/utils/i18n/routing/preview-list-page.ts
+++ src/utils/i18n/routing/preview-list-page.ts
@@ -3,21 +3,41 @@ import type { PaginateFunction } from "astro"
 import type { CollectionEntry, ContentCollectionKey } from "astro:content"
 import { groupByLang } from "../collection"
 
-type I18nPaginateArgs<C extends ContentCollectionKey> = {
+type PaginateOptions = {
   paginate: PaginateFunction
-  collection: CollectionEntry<C>[]
+  props?: Record<string, unknown>
+  params?: Record<string, string>
+}
+
+export const i18nPaginatePerLang = <C extends ContentCollectionKey>(
+  lang: string,
+  collection: CollectionEntry<C>[],
+  { paginate, params, props }: PaginateOptions
+) => {
+  const i18nPaths = paginate(collection, { pageSize: PAGE_SIZE, params: { lang, ...params }, props })
+
+  if (lang === "ja") {
+    const defaultPaths = paginate(collection, { pageSize: PAGE_SIZE, params: { lang: undefined, ...params }, props })
+    return [...i18nPaths, ...defaultPaths]
+  }
+
+  return i18nPaths
+}
+
+type I18nPaginateArgs = {
+  paginate: PaginateFunction
+  props?: Record<string, unknown>
+  params?: Record<string, string>
 }
 
 // [...lang]/**/[...page].astro用
-export const i18nPaginate = <C extends ContentCollectionKey>({ paginate, collection }: I18nPaginateArgs<C>) => {
+export const i18nPaginate = <C extends ContentCollectionKey>(
+  collection: CollectionEntry<C>[],
+  { paginate, props = {}, params = {} }: I18nPaginateArgs
+) => {
   const byLang = groupByLang(collection)
 
-  const jaCollection = collection.filter((page) => page.slug.startsWith("ja"))
-  const defaultPages = paginate(jaCollection, { pageSize: PAGE_SIZE, params: { lang: undefined } })
-
-  const byLangPages = [...byLang].flatMap(([lang, langPages]) => {
-    return paginate(langPages, { pageSize: PAGE_SIZE, params: { lang } })
+  return [...byLang].flatMap(([lang, langPages]) => {
+    return i18nPaginatePerLang(lang, langPages, { paginate, params, props })
   })
-
-  return [...defaultPages, ...byLangPages]
 }
