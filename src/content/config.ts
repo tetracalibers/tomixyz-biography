6f22b67444155db4c4e648be274fcb8b31af45b5
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-11-02 09:30:05 +0900

    eventコレクションのimageプロパティをastro:assets対応に

--- src/content/config.ts
+++ src/content/config.ts
@@ -33,16 +33,17 @@ const projectCollection = defineCollection({
 })
 
 const eventCollection = defineCollection({
-  schema: z.object({
-    title: z.string().max(100, "The title length must be less than or equal to 100 chars"),
-    description: z.string(),
-    date: z.string(),
-    image: z.string().optional(),
-    url: z.string().url().optional(),
-    slide: z.string().url().optional(),
-    archive: z.string().url().optional(),
-    github: z.string().url().optional()
-  })
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().max(100, "The title length must be less than or equal to 100 chars"),
+      description: z.string(),
+      date: z.string(),
+      image: image().optional(),
+      url: z.string().url().optional(),
+      slide: z.string().url().optional(),
+      archive: z.string().url().optional(),
+      github: z.string().url().optional()
+    })
 })
 
 export const collections = {
