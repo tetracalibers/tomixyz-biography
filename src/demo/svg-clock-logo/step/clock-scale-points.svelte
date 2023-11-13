055e29b0169c89bb71f6b1d2f599a2d41cc81b6f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-11-13 21:06:10 +0900

    時計の目盛りだけを表示したデモ作成

---
+++ src/demo/svg-clock-logo/step/clock-scale-points.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { cx, cy, r } from "../config"
+  import { calcArcLength, fitViewbox } from "../utils"
+</script>
+
+<svg width="300" height="300" use:fitViewbox={{ padding: 10 }}>
+  <!-- 時計の目盛り -->
+  <g fill="none" stroke="black" stroke-linecap="round">
+    <!-- 1分ごとの目盛り -->
+    <circle cx={cx} cy={cy} r={r} stroke-width="4" stroke-dasharray={`0.01 ${calcArcLength(r, 6)}`}></circle>
+    <!-- 5分ごとの目盛り -->
+    <circle cx={cx} cy={cy} r={r} stroke-width="8" stroke-dasharray={`0.1 ${calcArcLength(r, 30)}`}></circle>
+  </g>
+</svg>
