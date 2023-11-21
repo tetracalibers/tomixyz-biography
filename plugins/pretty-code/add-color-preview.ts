50cdc198b83e21d59d099f883028e4ca373b6df4
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-11-22 07:22:22 +0900

    pattern-matchingっぽく整理

--- plugins/pretty-code/add-color-preview.ts
+++ plugins/pretty-code/add-color-preview.ts
@@ -100,6 +100,51 @@ const prependColorPreviewElement = (color: string, $token: Element) => {
   $token.children.unshift(colorPreview)
 }
 
+type MatchResult =
+  | { type: "INVALID"; color: null }
+  | {
+      type: "TOKEN_IS_COLOR" | "TOKEN_HAS_COLOR" | "MERGED_TOKEN_IS_COLOR"
+      color: string
+    }
+
+const matchMaybeHexToken = (token: string, index: number, $parent: ElementContent[]): MatchResult => {
+  if (validateHTMLColorHex(token)) {
+    return { type: "TOKEN_IS_COLOR", color: token }
+  }
+
+  // トークンを読み進めて、連結したらhexの文字列になるかどうかを判定する
+  const maybeHex = walkValidateHTMLColorHex($parent, index)
+
+  if (maybeHex) {
+    return { type: "MERGED_TOKEN_IS_COLOR", color: maybeHex }
+  }
+
+  return { type: "INVALID", color: null }
+}
+
+const matchMaybeRgbToken = (token: string, index: number, $parent: ElementContent[]): MatchResult => {
+  if (validateHTMLColorRgb(token)) {
+    return { type: "TOKEN_IS_COLOR", color: token }
+  }
+
+  // トークンを読み進めて、連結したらrgb()の文字列になるかどうかを判定する
+  const maybeRgb = walkValidateHTMLColorRgb($parent, token, index)
+
+  if (maybeRgb) {
+    return { type: "MERGED_TOKEN_IS_COLOR", color: maybeRgb }
+  }
+
+  return { type: "INVALID", color: null }
+}
+
+const matchMaybeColorNameToken = (token: string): MatchResult => {
+  if (validateHTMLColorName(token)) {
+    return { type: "TOKEN_IS_COLOR", color: token }
+  }
+
+  return { type: "INVALID", color: null }
+}
+
 export const addColorPreview = (element: LineElement) => {
   const tokens = element.children
 
@@ -111,29 +156,43 @@ export const addColorPreview = (element: LineElement) => {
 
     const { value, raw } = values
 
-    let color = value
-
-    if (value.startsWith("#")) {
-      const isHexString = validateHTMLColorHex(value)
-      // トークンを読み進めて、連結したらhexの文字列になるかどうかを判定する
-      if (!isHexString) {
-        const maybeHex = walkValidateHTMLColorHex(tokens, i)
-        if (maybeHex === null) return
-        color = maybeHex
-      }
-    } else if (value.startsWith("rgb")) {
-      const isRgbString = validateHTMLColorRgb(value)
-      // トークンを読み進めて、連結したらrgb()の文字列になるかどうかを判定する
-      if (!isRgbString) {
-        const maybeRgb = walkValidateHTMLColorRgb(tokens, value, i)
-        if (maybeRgb === null) return
-        color = maybeRgb
-      }
-    } else {
-      if (!validateHTMLColorName(value)) return
+    // トークン全体が色コードの場合に実行する関数
+    const onTokenIsColorCode = (color: string) => {
+      convertSpaceToElement(raw, token, i, tokens)
+      prependColorPreviewElement(color, token)
     }
 
-    convertSpaceToElement(raw, token, i, tokens)
-    prependColorPreviewElement(color, token)
+    // 複数先まで先読みしてトークンを連結すると色コードになる場合に実行する関数
+    const onMergedTokenIsColorCode = (color: string) => {
+      convertSpaceToElement(raw, token, i, tokens)
+      prependColorPreviewElement(color, token)
+    }
+
+    // トークン中に色コードが含まれる場合に実行する関数
+    const onTokenHasColorCode = (color: string) => {}
+
+    let result: MatchResult = { type: "INVALID", color: null }
+
+    if (value.startsWith("#")) {
+      result = matchMaybeHexToken(value, i, tokens)
+    } else if (value.startsWith("rgb")) {
+      result = matchMaybeRgbToken(value, i, tokens)
+    } else {
+      result = matchMaybeColorNameToken(value)
+    }
+
+    switch (result.type) {
+      case "TOKEN_IS_COLOR":
+        onTokenIsColorCode(result.color)
+        break
+      case "TOKEN_HAS_COLOR":
+        onTokenHasColorCode(result.color)
+        break
+      case "MERGED_TOKEN_IS_COLOR":
+        onMergedTokenIsColorCode(result.color)
+        break
+      case "INVALID":
+        break
+    }
   })
 }
