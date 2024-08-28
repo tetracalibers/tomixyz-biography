2cb8908f492282a411da3e84e63e4fdb1cf7b590
tetracalibers <tomix05may@gmail.com>
2024-08-28 09:59:42 +0900

    baseの設定を元に戻す

--- astro.config.mjs
+++ astro.config.mjs
@@ -46,7 +46,7 @@ export default defineConfig(
     // outDir: './dist',       // When running `astro build`, path to final static output
     // publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
     site: "https://tetracalibers.github.io",
-    base: import.meta.env.CF_PAGES == 1 ? "" : "/tomixyz-biography",
+    base: "/tomixyz-biography",
     trailingSlash: "ignore",
     scopedStyleStrategy: "where",
     server: {
